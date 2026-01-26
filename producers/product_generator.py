8ca22d8c7c6d95113e202f227379e81373acff5b
Aritra Lahiri <aritralahiri58@gmail.com>
2026-01-27 00:10:58 +0530

    Added product dimension generator

---
+++ producers/product_generator.py
@@ -0,0 +1,7 @@
+import json
+import random
+
+product_name = ["Laptop", "Smartphone", "Headphones", "Tablet", "Smartwatch",
+    "Camera", "Printer", "Monitor", "Keyboard", "Mouse"]
+
+Categories = ["Electronics", "Accessories", "Computing", "Gadgets"]
